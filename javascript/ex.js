bd251ee6f417e7bfdd8e11225925474363f02b20
Arya0Patil <62802742+Arya0Patil@users.noreply.github.com>
2022-02-08 14:27:23 +0530

    js

---
+++ javascript/ex.js
@@ -0,0 +1,29 @@
+let number = prompt("h",8)
+
+number %2==0 ? "iam even":"odd" //ternary operator
+
+
+// if else
+
+if(true){
+    console.log("true")
+}else{
+    console.log("false")
+}
+
+// switch case 
+let num=10;
+
+switch(num) {
+  case 10:
+    // code block
+    console.log("a")
+    break;
+  case 12:
+console.log("b")
+    // code block
+    break;
+  default:
+console.log("no val")
+    // code block
+}
